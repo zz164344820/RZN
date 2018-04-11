017773ad92e7b1dbf2074c23614f9bcea4b97f40
zz <164344820@qq.com>
2018-04-11 17:49:25 +0800

    新建机手作业订单筛选Fragment

---
+++ module_driver/src/main/java/com/rzn/module_driver/ui/jobOrder/myjoborder/MyjobOrderActivity.java
@@ -0,0 +1,14 @@
+package com.rzn.module_driver.ui.jobOrder.myjoborder;
+
+
+import com.rzn.commonbaselib.mvp.MVPBaseActivity;
+
+
+/**
+ * MVPPlugin
+ *  邮箱 784787081@qq.com
+ */
+
+public class MyjobOrderActivity extends MVPBaseActivity<MyjobOrderContract.View, MyjobOrderPresenter> implements MyjobOrderContract.View {
+
+}
---
+++ module_driver/src/main/java/com/rzn/module_driver/ui/jobOrder/myjoborder/MyjobOrderPresenter.java
@@ -0,0 +1,12 @@
+package com.rzn.module_driver.ui.jobOrder.myjoborder;
+
+import com.rzn.commonbaselib.mvp.BasePresenterImpl;
+
+/**
+ * MVPPlugin
+ *  邮箱 784787081@qq.com
+ */
+
+public class MyjobOrderPresenter extends BasePresenterImpl<MyjobOrderContract.View> implements MyjobOrderContract.Presenter{
+    
+}
