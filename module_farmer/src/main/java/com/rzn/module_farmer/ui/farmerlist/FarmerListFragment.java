0ff6dbf60dafbf2335b0c23ccfa3ab3a514ab19c
lilei <176622729@qq.com>
2018-04-21 20:50:14 +0800

    添加机手fragment

---
+++ module_farmer/src/main/java/com/rzn/module_farmer/ui/farmerlist/FarmerListFragment.java
@@ -0,0 +1,27 @@
+package com.rzn.module_farmer.ui.farmerlist;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rzn.commonbaselib.mvp.MVPBaseFragment;
+import com.rzn.module_farmer.R;
+
+/**
+ * MVPPlugin
+ * 邮箱 784787081@qq.com
+ */
+
+public class FarmerListFragment extends MVPBaseFragment<FarmerListContract.View, FarmerListPresenter> implements FarmerListContract.View {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.farmer_fragment_farmerlist, container, false);
+        return rootView;
+    }
+}
---
+++ module_farmer/src/main/java/com/rzn/module_farmer/ui/farmerlist/FarmerListPresenter.java
@@ -0,0 +1,12 @@
+package com.rzn.module_farmer.ui.farmerlist;
+
+import com.rzn.commonbaselib.mvp.BasePresenterImpl;
+
+/**
+ * MVPPlugin
+ *  邮箱 784787081@qq.com
+ */
+
+public class FarmerListPresenter extends BasePresenterImpl<FarmerListContract.View> implements FarmerListContract.Presenter{
+    
+}
