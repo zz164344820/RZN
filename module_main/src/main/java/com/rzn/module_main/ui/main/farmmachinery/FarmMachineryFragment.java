901034970c2c4b88f7f7876da625f73659124db0
lilei <176622729@qq.com>
2018-05-20 20:50:17 +0800

    修改布局

--- module_main/src/main/java/com/rzn/module_main/ui/main/farmmachinery/FarmMachineryFragment.java
+++ module_main/src/main/java/com/rzn/module_main/ui/main/farmmachinery/FarmMachineryFragment.java
@@ -1,7 +1,14 @@
 package com.rzn.module_main.ui.main.farmmachinery;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.rzn.commonbaselib.mvp.MVPBaseFragment;
+import com.rzn.module_main.R;
 
 /**
  * MVPPlugin
@@ -9,5 +16,11 @@ import com.rzn.commonbaselib.mvp.MVPBaseFragment;
  */
 
 public class FarmMachineryFragment extends MVPBaseFragment<FarmMachineryContract.View, FarmMachineryPresenter> implements FarmMachineryContract.View {
-
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+//        inflater.inflate(R.layout.)
+//        return rootView;
+    }
 }
--- module_main/src/main/java/com/rzn/module_main/ui/main/mine/MineFragment.java
+++ module_main/src/main/java/com/rzn/module_main/ui/main/mine/MineFragment.java
@@ -6,21 +6,79 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.rzn.commonbaselib.mvp.MVPBaseFragment;
 import com.rzn.module_main.R;
 
+import io.reactivex.internal.operators.observable.ObservableNever;
+
 /**
  * MVPPlugin
- *  邮箱 784787081@qq.com
+ * 邮箱 784787081@qq.com
  */
 
 public class MineFragment extends MVPBaseFragment<MineContract.View, MinePresenter> implements MineContract.View {
     View rootView;
+    private LinearLayout llMyWork;
+    private LinearLayout llMyCollection;
+    private LinearLayout llAdvice;
+    private LinearLayout llPhoneConcel;
+    private LinearLayout llSetting;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        rootView=   inflater.inflate(R.layout.act_myself,container,false);
+        rootView = inflater.inflate(R.layout.act_myself, container, false);
+        initViews();
+        initListener();
         return rootView;
+    }
+
+    private void initListener() {
+        llAdvice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        llMyCollection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        llMyWork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        llPhoneConcel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        llSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+
+    private void initViews() {
+
+        llMyWork = (LinearLayout) rootView.findViewById(R.id.ll_my_work);
+        llMyCollection = (LinearLayout) rootView.findViewById(R.id.ll_my_collection);
+        llAdvice = (LinearLayout) rootView.findViewById(R.id.ll_advice);
+        llPhoneConcel = (LinearLayout) rootView.findViewById(R.id.ll_phone_concel);
+        llSetting = (LinearLayout) rootView.findViewById(R.id.ll_setting);
+
     }
 }
