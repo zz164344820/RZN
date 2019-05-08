b780772b838a45c0403859b5e07b3af0bd3e4db1
zz <164344820@qq.com>
2019-05-08 21:04:22 +0800

    添加卖农货UI

--- module_main/src/main/java/com/rzn/module_main/ui/sellagriculturalgoods/SellAgriculturalGoodsActivity.java
+++ module_main/src/main/java/com/rzn/module_main/ui/sellagriculturalgoods/SellAgriculturalGoodsActivity.java
@@ -37,6 +37,8 @@ public class SellAgriculturalGoodsActivity extends MVPBaseActivity<SellAgricultu
     ViewPager viewPager;
    TagFlowLayout id_flowlayout;
     TextView tv_classify;
+    CustomPopWindow mCustomPopWindow;
+    View line_gray;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,7 +54,7 @@ public class SellAgriculturalGoodsActivity extends MVPBaseActivity<SellAgricultu
         tabLayout=(MagicIndicator)findViewById(R.id.viewpagertab);
         viewPager=(ViewPager)findViewById(R.id.viewPager);
         tv_classify=(TextView)findViewById(R.id.tv_classify);
-       //
+        line_gray=(View)findViewById(R.id.line_gray);
     }
 
 
@@ -60,34 +62,47 @@ public class SellAgriculturalGoodsActivity extends MVPBaseActivity<SellAgricultu
     public void setViewPager(List<String> list, ArrayList<CommodityListFragment> fragmentList) {
         final LayoutInflater mInflater = LayoutInflater.from(this);
         viewPager.setAdapter(new CommodityPageAdapter(getSupportFragmentManager(),fragmentList));
-        //String[] array1 = list.toArray(new String[list.size()]);
         CommonNavigator commonNavigator = new CommonNavigator(this);
         commonNavigator.setAdapter(new CommodityAdapter(this,viewPager,list));
         tabLayout.setNavigator(commonNavigator);
         ViewPagerHelper.bind(tabLayout, viewPager);
+        setPopWindow(list, mInflater);
+    }
+
+
+
+    private void setPopWindow(final List<String> list, final LayoutInflater mInflater) {
         id_flowlayout= (TagFlowLayout)mInflater.inflate(R.layout.goodsclassift_popwindow,null);
-       // id_flowlayout=(TagFlowLayout)view.findViewById(R.id.id_flowlayout);
         tv_classify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CustomPopWindow mCustomPopWindow= new CustomPopWindow.PopupWindowBuilder(SellAgriculturalGoodsActivity.this)
+                     mCustomPopWindow= new CustomPopWindow.PopupWindowBuilder(SellAgriculturalGoodsActivity.this)
                         .setView(id_flowlayout)
                         .enableBackgroundDark(true) //弹出popWindow时，背景是否变暗
                         .setBgDarkAlpha(0.9f) // 控制亮度
                         .size(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)//显示大小
                         .create()
-                        .showAsDropDown(tv_classify,0,20);
+                        .showAsDropDown(line_gray,0,0);
+                id_flowlayout.getAdapter().setSelectedList(viewPager.getCurrentItem());
             }
         });
 
-       id_flowlayout.setAdapter(new TagAdapter<String>(list) {
+        id_flowlayout.setAdapter(new TagAdapter<String>(list) {
+             @Override
+             public View getView(FlowLayout parent, int position, String s)
+             {
+                 TextView tv = (TextView) mInflater.inflate(R.layout.commodityclassify,
+                         id_flowlayout, false);
+                 tv.setText(s);
+                 return tv;
+             }
+         });
+
+        id_flowlayout.setOnTagClickListener(new TagFlowLayout.OnTagClickListener() {
             @Override
-            public View getView(FlowLayout parent, int position, String s)
-            {
-                TextView tv = (TextView) mInflater.inflate(R.layout.commodityclassify,
-                        id_flowlayout, false);
-                tv.setText(s);
-                return tv;
+            public boolean onTagClick(View view, int position, FlowLayout parent) {
+                tabLayout.onPageSelected(position);
+                return false;
             }
         });
     }
